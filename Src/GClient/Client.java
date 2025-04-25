4c2941824ac8a40c17e0bfbeac27405859a23ee0
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-04-25 23:01:38 +0700

    Add files via upload

--- Src/GClient/Client.java
+++ Src/GClient/Client.java
@@ -1,1 +1,22 @@
+package gclient;
 
+import org.lwjgl.opengl.Display;
+
+import gclient.gui.GClientSplashScreen;
+
+public class Client {
+    public String name = "Gclient ";
+    public String version = "1.8.9 ";
+    public String build = "Build 1030";
+	public static Client INSTANCE = new Client();
+	
+	
+	public void startup() {
+		GClientSplashScreen.setProgress(1, "GClient -Initializing");
+		Display.setTitle(name + version + build); 
+	}
+	
+	public void shutdown() {
+		
+	}
+}
